761b04ba5f0dc90145416d427479c2ecf7b27a68
Alicture <lzp123518@gmail.com>
2018-08-27 23:14:46 +0800

    clean code

--- EZBUTTON_Arduino/EZBUTTON.ino
+++ EZBUTTON_Arduino/EZBUTTON.ino
@@ -7,26 +7,15 @@ void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   digitalWrite(buttonPin, HIGH);
   Serial.begin(9600);
-//  Keyboard.begin();
 }
-
 void loop() {
   buttonState = digitalRead(buttonPin);
-//  delay(2000);
   if ((flag==true)&&(buttonState != prevButtonState) && (buttonState == HIGH)) {
-    // Here starts the output action
-//    Keyboard.press(KEY_LEFT_GUI); // Command key in Mac, use KEY_LEFT_CTRL for Pc
-//    Keyboard.press('v');
     Serial.println("1");
-    
     flag=false;
     }
-//    Keyboard.releaseAll(); // This is important after every Keyboard.press it will continue to be pressed
   else if((buttonState == prevButtonState) && (buttonState != HIGH)&&(flag==false)){
     Serial.println("0");
-//    Serial.println(buttonState);
-//    Serial.println(prevButtonState);
-//    delay(1000);
     flag=true;
     delay(100);
   }
